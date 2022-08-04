f3e95466991e1c7d59f80f0f5b84650428388b1e
user name <bkcsplayer@gmail.com>
2022-08-03 22:37:01 -0600

    first commit 01

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,15 +11,17 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1LSuwNJQ1xeWqtvYzqhRs5vU' },
+          { shipping_rate: 'shr_1LSuzNJQ1xeWqtvYjogqqRvV' },
+          { shipping_rate: 'shr_1LSuzmJQ1xeWqtvYNf3c5ABN' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/cg8tlnay/production/').replace('-webp', '.webp');
 
           return {
             price_data: { 
-              currency: 'usd',
+              currency: 'cad',
               product_data: { 
                 name: item.name,
                 images: [newImage],
